f761429dc0e23886570e79cee551054050f7b8fa
Yessen Murat <shoyo2209@gmail.com>
2023-01-07 16:23:24 +0600

    Created form to submit a task and stored in local storage

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 const createTaskBtn = document.getElementById("create-task");
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("close-modal");
+const saveTaskForm = document.getElementById("saveTaskForm");
 let isModalOpen = false;
+
+let TASKS = [];
+
+window.addEventListener("DOMContentLoaded", () => {
+  getTasks();
+});
+
+function getTasks() {
+  TASKS = JSON.parse(localStorage.tasks);
+}
 
 createTaskBtn.onclick = () => {
   modal.style.display = "block";
@@ -21,3 +32,33 @@ const toggleModal = () => {
   const display = isModalOpen ? "block" : "none";
   modal.style.display = display;
 };
+
+saveTaskForm.onsubmit = (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const formProps = Object.fromEntries(formData);
+
+  const task = new Task(formProps.title, formProps.description);
+  task.saveTask();
+};
+
+class Task {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.created = new Date().toISOString();
+  }
+
+  saveTask() {
+    const newTask = {
+      title: this.title,
+      description: this.description,
+      created: this.created,
+    };
+
+    getTasks();
+    TASKS.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(TASKS));
+    toggleModal();
+  }
+}
