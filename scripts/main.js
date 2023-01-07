9e396910f0bcab23c41d1448b1f88d6f7f0b0253
Yessen Murat <shoyo2209@gmail.com>
2023-01-07 20:29:00 +0600

    Display tasks from local storage

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,24 @@ const createTaskBtn = document.getElementById("create-task");
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("close-modal");
 const saveTaskForm = document.getElementById("saveTaskForm");
+const taskListContainer = document.querySelector(".task-list-container");
 let isModalOpen = false;
 
 let TASKS = [];
 
 window.addEventListener("DOMContentLoaded", () => {
   getTasks();
+  displayTasks();
 });
 
 function getTasks() {
   TASKS = localStorage.tasks ? JSON.parse(localStorage.tasks) : [];
+}
+
+function displayTasks() {
+  for (const task of TASKS) {
+    taskListContainer.appendChild(addTaskCard(task));
+  }
 }
 
 createTaskBtn.onclick = () => {
@@ -42,6 +50,22 @@ saveTaskForm.onsubmit = (event) => {
   task.saveTask();
 };
 
+// Components
+const addTaskCard = (task) => {
+  const taskCard = document.createElement("article");
+  const title = document.createElement("h3");
+  const description = document.createElement("p");
+
+  title.textContent = task.title;
+  description.textContent = task.description;
+  taskCard.classList.add("task-card");
+  taskCard.appendChild(title);
+  taskCard.appendChild(description);
+
+  return taskCard;
+};
+
+// Classes
 class Task {
   constructor(title, description) {
     this.title = title;
